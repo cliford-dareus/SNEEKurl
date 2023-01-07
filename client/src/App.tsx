1532ee3ce2c2d0e4b85f3d291cdc90be1242c39e
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2023-01-07 12:35:38 -0500

    setup server side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,6 @@ function App() {
     <div className="App">
     </div>
   )
-}
+};
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 dotenv.config();
 
 const app = express();
 
+app.use(morgan('combined'));
+app.use(cors());
+app.use(helmet());
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+start();
+
