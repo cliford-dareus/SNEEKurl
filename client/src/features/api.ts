90bff8989d2890fc4625347e74622747cd0f9abe
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2023-01-08 19:20:53 -0500

    setup apiSlice

---
+++ client/src/features/api.ts
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+    reducerPath: 'UrlApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:4080'
+    }),
+    endpoints: (build) => ({
+        registerUser: build.mutation({
+            query:(body) => ({
+                url: '/api/v1/auth/register',
+                method: 'POST',
+                body
+            })
+        }),
+        loginUser: build.mutation({
+            query:(body) => ({
+                url: '/api/v1/auth/login',
+                method: 'POST',
+                body
+            })
+        })
+    })
+});
+
+export const { useRegisterUserMutation, useLoginUserMutation } = apiSlice;
