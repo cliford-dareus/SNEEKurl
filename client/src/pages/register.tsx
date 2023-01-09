4b5e300c4f9f6690137beb5df260647315f0c3a1
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2023-01-08 19:55:32 -0500

    setup register user

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import InputForm from '../components/InputForm';
+import { useRegisterUserMutation } from '../features/api';
 
 interface UserInterface {
     name: string;
@@ -8,16 +9,31 @@ interface UserInterface {
 };
 
 const Register = () => {
+    const [ registerUser, { isLoading }] = useRegisterUserMutation();
     const [ userInfo, setUserInfo ] = useState<UserInterface>({ name: '', email: '', password: '' });
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setUserInfo({...userInfo, [event.target.name] : event.target.value })
     };
 
+    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const { name, email, password } = userInfo;
+        const body = { name, email, password };
+
+        try {
+            registerUser(body);
+            setUserInfo({ name: '', email: '', password: '' });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
   return (
     <div className='w-4/12 flex flex-col items-center'>
         <h2 className='text-2xl text-white'>Register</h2>
         <form 
+            onSubmit={onSubmit}
             action=""
             className='w-full flex flex-col gap-4 mt-8 bg-blue-900 py-8 px-6 rounded-md'
         >
